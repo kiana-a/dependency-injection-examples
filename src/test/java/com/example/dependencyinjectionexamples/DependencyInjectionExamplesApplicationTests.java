3908a5ed5612e21ee5501e2a48539c085dabdec0
Kiana <kiyana.ahmadi@gmail.com>
2020-08-06 15:12:22 +0430

    initial commit

---
+++ src/test/java/com/example/dependencyinjectionexamples/DependencyInjectionExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dependencyinjectionexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyInjectionExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
