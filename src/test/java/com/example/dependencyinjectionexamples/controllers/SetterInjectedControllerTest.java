4ed3a5ba41b350d39d28eb04b2551c8bb07cd1ea
Kiana <kiyana.ahmadi@gmail.com>
2020-08-06 16:36:38 +0430

    setter injection

---
+++ src/test/java/com/example/dependencyinjectionexamples/controllers/SetterInjectedControllerTest.java
@@ -0,0 +1,23 @@
+package com.example.dependencyinjectionexamples.controllers;
+
+import com.example.dependencyinjectionexamples.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SetterInjectedControllerTest {
+
+    SetterInjectedController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new SetterInjectedController();
+        controller.setGreetingService(new GreetingServiceImpl());
+    }
+
+    @Test
+    void setGreetingService() {
+        System.out.println(controller.getGreeting());
+    }
+}
